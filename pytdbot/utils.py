f375a839fb1e6c7ab55c7fea68a775adffb47616
AYMEN Mohammed <53928879+AYMENJD@users.noreply.github.com>
2022-07-16 16:41:12 +0300

    Minor improvements.

--- pytdbot/utils.py
+++ pytdbot/utils.py
@@ -1,26 +1,39 @@
-from html import escape as _h_escape
-from re import escape as _re_escape, sub
+from html import escape as _html_escape
+from re import escape as _re_escape, compile as _re_compile
 
 
 def escape_html(text: str, quote: bool = True) -> str:
     """Escape HTML characters in the given text.
 
     Args:
-        text (``str``): The text to escape.
-        quote (``bool``, optional): Whether to escape double quotes. Defaults to True.
+        text (``str``):
+            The text to escape.
+
+        quote (``bool``, optional):
+            Whether to escape double quotes. Defaults to True.
 
     Returns:
         ``str``: The escaped text.
     """
-    return _h_escape(text, quote=quote)
+    assert isinstance(text, str), "text must be a string"
+    assert isinstance(quote, bool), "quote must be a boolean"
+
+    return _html_escape(text, quote=quote)
+
+
+special_chars_v1 = _re_compile("([{}])".format(_re_escape(r"_\*`\[")))
+special_chars_v2 = _re_compile("([{}])".format(_re_escape(r"\_*[]()~`>#+-=|{}.!")))
 
 
 def escape_markdown(text: str, version: int = 1) -> str:
     """Escape Markdown characters in the given text.
 
     Args:
-        text (``str``): The text to escape.
-        version (``int``, optional): The Markdown version to escape. Defaults to 1.
+        text (``str``):
+            The text to escape.
+
+        version (``int``, optional):
+            The Markdown version to escape. Defaults to 1.
 
     Returns:
         ``str``: The escaped text.
@@ -28,11 +41,13 @@ def escape_markdown(text: str, version: int = 1) -> str:
     Raises:
         ``ValueError``: If the given markdown version is not supported.
     """
+    assert isinstance(text, str), "text must be a string"
+
     if version == 1:
-        chars = _re_escape(r"_\*`\[")
+        chars = special_chars_v1
     elif version == 2:
-        chars = _re_escape(r"\_*[]()~`>#+-=|{}.!")
+        chars = special_chars_v2
     else:
         raise ValueError("Invalid version. Must be 1 or 2.")
 
-    return sub(f"([{chars}])", r"\\\1", text)
+    return chars.sub(r"\\\1", text)
