a9c97e7090ae266016efe6571a89980aa184961f
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2023-06-25 09:40:14 +0300

    Bump dev version to 0.8.4dev3.

--- pytdbot/__init__.py
+++ pytdbot/__init__.py
@@ -2,7 +2,7 @@ from . import types, utils, filters, exception
 from .tdjson import TdJson
 from .client import Client
 
-__version__ = "0.8.4dev2"
+__version__ = "0.8.4dev3"
 __copyright__ = "Copyright (c) 2022-2023 AYMEN Mohammed ~ https://github.com/AYMENJD"
 __license__ = "MIT License"
 
