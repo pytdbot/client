1c66d08fcf9757e5d58aef903690677fa8494d49
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2024-05-26 20:24:11 +0300

    Update version to 0.8.8.

--- pytdbot/__init__.py
+++ pytdbot/__init__.py
@@ -2,7 +2,7 @@ from . import types, utils, filters, exception
 from .tdjson import TdJson
 from .client import Client
 
-__version__ = "0.8.8dev0"
+__version__ = "0.8.8"
 __copyright__ = "Copyright (c) 2022-2023 AYMEN Mohammed ~ https://github.com/AYMENJD"
 __license__ = "MIT License"
 
