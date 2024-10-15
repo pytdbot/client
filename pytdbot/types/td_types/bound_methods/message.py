5bfcbe6738d11cfefc9d02cff9411635478d89bb
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2024-10-16 01:20:47 +0300

    Add remote_file_id and remote_unique_id to Message.

--- pytdbot/types/td_types/bound_methods/message.py
+++ pytdbot/types/td_types/bound_methods/message.py
@@ -66,6 +66,56 @@ class MessageBoundMethods:
         ):
             return self.content.caption.text
 
+    @property
+    @lru_cache(1)
+    def remote_file_id(self) -> Union[str, None]:
+        """Remote file id"""
+
+        file_id = None
+        if isinstance(self.content, pytdbot.types.MessagePhoto):
+            file_id = self.content.photo.sizes[-1].photo.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageVideo):
+            file_id = self.content.video.video.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageSticker):
+            file_id = self.content.sticker.sticker.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageAnimation):
+            file_id = self.content.animation.animation.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageAudio):
+            file_id = self.content.audio.audio.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageDocument):
+            file_id = self.content.document.document.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageVoiceNote):
+            file_id = self.content.voice_note.voice.remote.id
+        elif isinstance(self.content, pytdbot.types.MessageVideoNote):
+            file_id = self.content.video_note.video.remote.id
+
+        return file_id
+
+    @property
+    @lru_cache(1)
+    def remote_unique_file_id(self) -> Union[str, None]:
+        """Remote unique file id"""
+
+        file_id = None
+        if isinstance(self.content, pytdbot.types.MessagePhoto):
+            file_id = self.content.photo.sizes[-1].photo.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageVideo):
+            file_id = self.content.video.video.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageSticker):
+            file_id = self.content.sticker.sticker.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageAnimation):
+            file_id = self.content.animation.animation.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageAudio):
+            file_id = self.content.audio.audio.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageDocument):
+            file_id = self.content.document.document.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageVoiceNote):
+            file_id = self.content.voice_note.voice.remote.unique_id
+        elif isinstance(self.content, pytdbot.types.MessageVideoNote):
+            file_id = self.content.video_note.video.remote.unique_id
+
+        return file_id
+
     async def mention(self, parse_mode: str = "markdownv2") -> str:
         """Get the text_mention of the message sender
 
