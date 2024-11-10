b87dccbaf5b5a79fa120f22c181ddb3c70a75308
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2024-11-11 02:13:10 +0300

    Update copyright year.

--- pytdbot/__init__.py
+++ pytdbot/__init__.py
@@ -5,7 +5,7 @@ from .client import Client
 __all__ = ["types", "utils", "filters", "exception", "TdJson", "Client"]
 
 __version__ = "0.9.0dev1"
-__copyright__ = "Copyright (c) 2022-2023 AYMEN Mohammed ~ https://github.com/AYMENJD"
+__copyright__ = "Copyright (c) 2022-2024 AYMEN Mohammed ~ https://github.com/AYMENJD"
 __license__ = "MIT License"
 
 VERSION = __version__
