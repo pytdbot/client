176e6c448e55e8b3e33185f1269b6c2147b75d46
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-01-03 22:15:59 +0300

    Improve copyright name.

--- pytdbot/__init__.py
+++ pytdbot/__init__.py
@@ -5,7 +5,7 @@ from .client import Client
 __all__ = ["types", "utils", "filters", "exception", "TdJson", "Client"]
 
 __version__ = "0.9.0dev6"
-__copyright__ = "Copyright (c) 2022-2025 AYMEN Mohammed ~ https://github.com/AYMENJD"
+__copyright__ = "Copyright (c) 2022-2025 Pytdbot, AYMENJD"
 __license__ = "MIT License"
 
 VERSION = __version__
