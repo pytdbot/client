2891309ac265492d1386cbe6734425461ce4bbee
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-03-19 10:13:21 +0300

    Update dev version to 0.9.2.dev0.

--- pytdbot/__init__.py
+++ pytdbot/__init__.py
@@ -4,7 +4,7 @@ from .client import Client
 
 __all__ = ["types", "utils", "filters", "exception", "TdJson", "Client"]
 
-__version__ = "0.9.1"
+__version__ = "0.9.2.dev0"
 __copyright__ = "Copyright (c) 2022-2025 Pytdbot, AYMENJD"
 __license__ = "MIT License"
 
