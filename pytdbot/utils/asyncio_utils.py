2986e456769910a66188626798946cf48c20b5f1
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-10-25 23:49:35 +0300

    Improve get_running_loop.

--- pytdbot/utils/asyncio_utils.py
+++ pytdbot/utils/asyncio_utils.py
@@ -2,9 +2,9 @@ import asyncio
 
 
 def get_running_loop():
-    """Get the current running event loop, or create a new one if none is running"""
+    """Get the current running event loop"""
 
     try:
         return asyncio.get_running_loop()
     except RuntimeError:
-        return asyncio.new_event_loop()
+        return asyncio.get_event_loop_policy().get_event_loop()
